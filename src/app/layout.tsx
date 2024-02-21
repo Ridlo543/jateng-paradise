9134ff8517cabd1aa4cdcefa684bf53421893bbb
Rizal Anggoro <gnoogler4@gmail.com>
2024-02-21 22:19:59 +0700

    fix: globals css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../styles/base.scss";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}
-      >
+      <body className={inter.className}>
         <main>{children}</main>
-        </body>
+      </body>
     </html>
   );
 }
