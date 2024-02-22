9614a4d6ea4ec6129b310e9793f426b1495893e4
Ridlo Abdullah Ulinnuha <ridloabdullahulinnuha543@gmail.com>
2024-02-22 23:55:17 +0700

    feat: add responsive navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <section className="relative h-screen">
         <Image
           src="/images/HeroBanner7.jpg"
-          layout="fill"
+          fill
           alt="Jateng Paradise Background"
           className="absolute z-0 object-cover w-full h-full"
           priority={true}
@@ -19,11 +19,12 @@ export default function Home() {
         <div className="absolute z-0 w-full h-full bg-black bg-opacity-20"></div>
         {/* Content */}
         <div className="flex flex-col items-center justify-center h-full z-20 relative text-center text-white px-4">
-          <h1 className="text-2xl font-bold sm:text-3xl md:text-4xl lg:text-5xl max-w-2xl mx-auto">
+          <h1 className=" font-bold text-4xl lg:text-5xl max-w-2xl mx-auto">
             Jelajahi Surga Tersembunyi
           </h1>
           <p className="text-md sm:text-lg md:text-xl mt-4 max-w-xl mx-auto">
-            Temukan Keajaiban Alam, Kekayaan Budaya, dan Warisan Sejarah yang Tak Ternilai di Jawa Tengah.
+            Temukan Keajaiban Alam, Kekayaan Budaya, dan Warisan Sejarah yang
+            Tak Ternilai di Jawa Tengah.
           </p>
 
           <Card className="flex items-center px-4 py-2 my-3 w-full max-w-md sm:max-w-lg md:max-w-xl lg:min-w-[480px]">
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
-import { AuthRepository } from "@/lib/repositories/auth";
+import React, { useEffect, useState } from "react";
 import { LogIn } from "lucide-react";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
@@ -15,10 +14,8 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { set } from "fp-ts";
 import { JWTPayload } from "jose";
 import { cn } from "@/lib/utils";
-import path from "path";
 
 type MenuItem = {
   title: string;
@@ -28,15 +25,21 @@ type MenuItem = {
 type NavbarComponentProps = {
   authPayload?: JWTPayload;
 };
+
 export default function NavbarComponent(props: NavbarComponentProps) {
   const pathname = usePathname();
   const [isAtTop, setIsAtTop] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const menus: Array<MenuItem> = [
     { title: "Home", href: "/" },
     { title: "About us", href: "/about" },
     { title: "Popular", href: "/popular" },
   ];
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   useEffect(() => {
     if (pathname !== "/") {
@@ -56,20 +59,17 @@ export default function NavbarComponent(props: NavbarComponentProps) {
   }, [pathname]);
 
   return (
-    <>
-      <div
+    <header className="header z-30 sticky top-0 ">
+      <nav
         className={cn(
-          "h-16 w-full fixed top-0  backdrop-blur-sm z-50 transition-all duration-300",
-          isAtTop ? "bg-transparent  " : "bg-white/70 "
+          "navbar container fixed  w-full transition-all duration-300 ",
+          isAtTop
+            ? "bg-transparent  backdrop-blur-sm"
+            : "bg-white bg-opacity-60 bg-clip-padding backdrop-filter backdrop-blur-xl"
         )}
       >
         {/* navbar container */}
-        <div
-          className={cn(
-            "flex justify-between h-16 max-w-[1024px] px-4 mx-auto items-center",
-            isAtTop ? "text-white" : ""
-          )}
-        >
+        <div className="flex justify-between h-16 max-w-[1024px] px-4 mx-auto items-center">
           {/* navbar title */}
           <Link href={"/"}>
             <Button
@@ -80,8 +80,43 @@ export default function NavbarComponent(props: NavbarComponentProps) {
             </Button>
           </Link>
 
+          {/* Navbar toggle button */}
+          <button
+            className={cn(
+              "md:hidden inline-flex items-center justify-center p-2 rounded-md cursor-pointer",
+              isAtTop ? "text-white" : ""
+            )}
+            onClick={toggleMenu}
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          >
+            <span className="sr-only">
+              {isMenuOpen ? "Close menu" : "Open menu"}
+            </span>
+            {isMenuOpen ? (
+              // Ikon untuk menu "Tutup"
+              <svg className="h-6 fill-current" viewBox="0 0 20 20">
+                <title>Menu Close</title>
+                <polygon
+                  points="11 9 22 9 22 11 11 11 11 22 9 22 9 11 -2 11 -2 9 9 9 9 -2 11 -2"
+                  transform="rotate(45 10 10)"
+                ></polygon>
+              </svg>
+            ) : (
+              // Ikon untuk menu "Buka"
+              <svg className="h-6 fill-current" viewBox="0 0 20 20">
+                <title>Menu Open</title>
+                <path d="M0 3h20v2H0V3z m0 6h20v2H0V9z m0 6h20v2H0V15z"></path>
+              </svg>
+            )}
+          </button>
+
           {/* navbar menus */}
-          <div>
+          <div
+            className={cn(
+              "hidden md:flex md:space-x-4",
+              isAtTop ? "text-white" : ""
+            )}
+          >
             {menus.map((item, index) => (
               <Link href={item.href} key={"navbar-menu-item-" + index}>
                 <Button
@@ -95,7 +130,7 @@ export default function NavbarComponent(props: NavbarComponentProps) {
           </div>
 
           {/* navbar auth actions */}
-          <div>
+          <div className="hidden md:flex md:items-center">
             {props.authPayload ? (
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
@@ -120,7 +155,45 @@ export default function NavbarComponent(props: NavbarComponentProps) {
             )}
           </div>
         </div>
-      </div>
-    </>
+        {/* Responsive Navbar Menu */}
+        {isMenuOpen && (
+          <div className="md:hidden flex flex-col items-center py-4">
+            {menus.map((item, index) => (
+              <Link href={item.href} key={"navbar-menu-item-" + index}>
+                <Button
+                  variant={"link"}
+                  className={cn(
+                    "py-2 px-4 my-1",
+                    isAtTop ? "text-white" : "text-black"
+                  )}
+                  onClick={toggleMenu}
+                >
+                  {item.title}
+                </Button>
+              </Link>
+            ))}
+
+            {/* Navbar auth actions for mobile */}
+            <div className="mt-4">
+              {props.authPayload ? (
+                // Show user avatar and logout option
+                <Avatar className="cursor-pointer">
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              ) : (
+                // Show login button
+                <Link href={"/auth/login"}>
+                  <Button size={"sm"} className="flex items-center">
+                    <LogIn className="w-4 h-4 mr-2" />
+                    Login
+                  </Button>
+                </Link>
+              )}
+            </div>
+          </div>
+        )}
+      </nav>
+    </header>
   );
 }
