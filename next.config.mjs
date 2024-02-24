7c4c585dced72d9e499f0681e87bf8c87ea56985
Rizal Anggoro <gnoogler4@gmail.com>
2024-02-24 17:44:49 +0700

    remove: unused dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
-import createMDX from "@next/mdx";
-import remarkGfm from "remark-gfm";
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-};
+const nextConfig = {};
 
-const withMDX = createMDX({
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [],
-  },
-});
-
-export default withMDX(nextConfig);
+export default nextConfig;
