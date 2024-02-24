e877bfe23e67a1ca0a2309f0ae2256977315d561
Ridlo Abdullah Ulinnuha <ridloabdullahulinnuha543@gmail.com>
2024-02-24 15:16:20 +0700

    feat: add favicon config all device

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,12 +12,12 @@ export const metadata: Metadata = {
   description: "Website Pariwisata Jawa Tengah",
 };
 
-export default async  function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-const authPayload = await AuthRepository.createSession();
+  const authPayload = await AuthRepository.createSession();
 
   return (
     <html lang="en">
@@ -26,6 +26,25 @@ const authPayload = await AuthRepository.createSession();
         <title>{String(metadata.title)}</title>
         {/* meta */}
         <meta name="description" content={metadata.description ?? ""} />
+        {/* icon */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={inter.className}>
         <main>
